b95d8bf5be72027ea5286a1f6c58dfdca2e47c5c
David Madl <git@abanbytes.eu>
2015-09-08 17:27:34 +0200

    first prefetch draft

--- util/probing_hash_table_benchmark_main.cc
+++ util/probing_hash_table_benchmark_main.cc
@@ -15,6 +15,8 @@
 #endif
 
 #include <iostream>
+#include <queue>
+#include <stdlib.h>
 
 namespace util {
 namespace {
@@ -25,7 +27,17 @@ struct Entry {
   Key GetKey() const { return key; }
 };
 
-template <class Mod> bool Test(uint64_t entries, uint64_t lookups = 20000000, float multiplier = 1.5) {
+template <class Table>
+struct BufferEntry {
+  typedef uint64_t Key;
+  typename Table::ConstIterator it;
+  Key k;
+  
+  BufferEntry(typename Table::ConstIterator it, Key k): it(it), k(k) {}
+  BufferEntry(): it(NULL), k(0) {}
+};
+
+template <class Mod> bool Test(uint64_t entries, int qsize, uint64_t lookups = 20000000, float multiplier = 1.5) {
   typedef util::ProbingHashTable<Entry, util::IdentityHash, std::equal_to<Entry::Key>, Mod> Table;
   // Always round up to power of 2 for fair comparison.
   std::size_t size = Power2Mod::RoundBuckets(Table::Size(entries, multiplier) / sizeof(Entry)) * sizeof(Entry);
@@ -41,26 +53,71 @@ template <class Mod> bool Test(uint64_t entries, uint64_t lookups = 20000000, fl
   }
   double inserted = UserTime();
   bool meaningless = true;
+  // gen addrs
+  uint64_t *keys = new uint64_t[lookups];
+  uint64_t *p = keys;
+  for (uint64_t i = 0; i < lookups; ++i)
+    keys[i] = dist(gen);
+
+  // benchmark find
+  double before_find = UserTime();
+  
+  std::queue<BufferEntry<Table> > q;
+  //const int qsize = 10;
+  
   for (uint64_t i = 0; i < lookups; ++i) {
     typename Table::ConstIterator it;
-    meaningless ^= table.Find(dist(gen), it);
+    BufferEntry<Table> be;
+    //meaningless ^= table.Find(*p++, it);
+    
+    if(q.size() == qsize) {
+      be = q.front();
+      meaningless ^= table.FindFromIterator(be.k, be.it);
+      q.pop();
+    }
+    it = table.Ideal(*p); // compute address in hash
+    q.push(BufferEntry<Table>(it, *p++));
+    __builtin_prefetch(it);
   }
-  std::cout << entries << ' ' << size << ' ' << (inserted - start) / static_cast<double>(entries) << ' ' << (UserTime() - inserted) / static_cast<double>(lookups) << '\n';
+  
+  // drain the queue
+  while(q.size()) {
+    typename Table::ConstIterator it;
+    BufferEntry<Table> be;
+    be = q.front();
+    meaningless ^= table.FindFromIterator(be.k, be.it);
+    q.pop();
+  }
+  
+  
+  /*
+  for (uint64_t i = 0; i < lookups; ++i) {
+    typename Table::ConstIterator it;
+    //meaningless ^= table.Find(*p++, it);
+    it = table.Ideal(*p);
+    meaningless ^= table.FindFromIterator(*p++, it);
+  }
+  */
+  std::cout << entries << ' ' << size << ' ' << (inserted - start) / static_cast<double>(entries) << ' ' << (UserTime() - before_find) / static_cast<double>(lookups) << '\n';
+
+  delete[] keys;
   return meaningless;
 }
 
 } // namespace
 } // namespace util
 
-int main() {
+int main(int argc, char** argv) {
+  int qsize = atoi(argv[1]);
+  
   bool meaningless = false;
   std::cout << "#Integer division\n";
   for (uint64_t i = 1; i <= 100000000ULL; i *= 10) {
-    meaningless ^= util::Test<util::DivMod>(i);
+    meaningless ^= util::Test<util::DivMod>(i, qsize);
   }
   std::cout << "#Masking\n";
   for (uint64_t i = 1; i <= 100000000ULL; i *= 10) {
-    meaningless ^= util::Test<util::Power2Mod>(i);
+    meaningless ^= util::Test<util::Power2Mod>(i, qsize);
   }
   std::cerr << "Meaningless: " << meaningless << '\n';
 }
