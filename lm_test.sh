775ceedd71aff31e329193c12929caa222571489
David Madl <git@abanbytes.eu>
2015-09-11 16:44:44 +0200

    lm test shellscripts

--- lm_test.sh
+++ lm_test.sh
@@ -3,9 +3,23 @@
 MOSES_PATH=/fs/lofn0/dmadl/software/mosesdecoder; MOSES=$MOSES_PATH; export PATH=$MOSES_PATH/bin:$MOSES_PATH/scripts:$PATH; WD=$(pwd);
 #ntest=200000
 
-for nprefetch in 1 2 5 10; do
-    for size in train.bin/*; do
-        #./test.sh $size $nprefetch 2>&1 >/dev/null | 
-        ./test.sh $size $nprefetch 2>/dev/null | awk '/total_runtime/ { print $3 }'
+test=bigcorpus.perm.en.tail
+
+for model in train.bin/*; do
+    echo $(basename $model)
+
+    # echo "Building vocab..." >&2
+    $MOSES/bin/kenlm_benchmark vocab $model < "$test" > /tmp/lm.vocab 2>/dev/null
+    
+    #Ensure files are in RAM.
+    cat /tmp/lm.vocab $model >/dev/null
+    
+    # no prefetch (old)
+    #./test.sh $model 2>/dev/null | awk '/total_runtime/ { print " " $3 }'
+    bin/kenlm_benchmark.old query $model < /tmp/lm.vocab 2>/dev/null | awk '/total_runtime/ { print " " $3 }'
+    for nprefetch in 1 2 5 10; do
+        # various prefetch models
+        #./test.sh $model $nprefetch 2>/dev/null | awk '/total_runtime/ { print " " $3 }'
+        bin/kenlm_benchmark query $model $nprefetch < /tmp/lm.vocab 2>/dev/null | awk '/total_runtime/ { print " " $3 }'
     done
 done
